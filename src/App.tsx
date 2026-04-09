e12b3eb78b04330386799e474dc1d5725d331f20
lptryan <ryan.thompson@lpt.com>
2026-04-09 19:00:21 -0400

    gravity fixes

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/buymyhouseproto/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
